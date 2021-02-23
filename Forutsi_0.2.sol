084bbe909ce54a8b55c18a6daf93b0f0cfc1cd49
unknown <orbi.dev@orbicular.io>
2021-02-23 00:56:03 -0500

    staticcall success

--- Forutsi_0.2.sol
+++ Forutsi_0.2.sol
@@ -8,18 +8,32 @@ contract Forutsi {
         
     }*/
     
-    function getProcess(bytes memory data) public view returns (bytes memory) {
+    function test(bytes memory predictionHex) public view {
         assembly {
             let ptr := mload(0x40)
-            mstore(ptr, 0x20)
-            let PO := mload(add(data, 0x80))
-            let PL := sub(mload(data), PO)
-            mstore(add(ptr, 0x20), PL)
+            let PO := mload(add(predictionHex, 0x80))
+            let PL := sub(mload(predictionHex), PO)
+            let predictionHexData := add(0x20, predictionHex)
             let i := 0
             for {} lt(i, PL) { i:= add(i, 0x20) } {
-                mstore(add(ptr, add(0x40, i)), mload(add(add(add(0x20, data), PO), i)))
+                mstore(add(ptr, i), mload(add(add(predictionHexData, PO), i)))
             }
-            return(ptr, add(add(PL, 0x60), i))
+            let P := ptr
+            ptr := add(ptr, PL)
+            let stepType := byte(0, mload(P))
+            mstore(0x40, add(ptr, 0x01))
+            switch stepType
+            case 0x00 {
+                let getIndex := mload(ptr)
+                ptr := add(predictionHexData, mload(add(predictionHexData, mload(add(predictionHexData, 0x20)))))
+                let to := shr(0x60, and(mload(ptr), 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF000000000000000000000000))
+                ptr := add(predictionHexData, mload(add(add(ptr, 0x14), 0x20)))
+                let callDataLen := mload(ptr)
+                ptr := add(ptr, 0x20)
+                pop(staticcall(gas(), to, ptr, callDataLen, mload(0x40), mload(add(ptr, callDataLen))))
+                ptr := mload(0x40)
+                
+            }
         }
     }
     
